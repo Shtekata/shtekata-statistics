388af8cb864a1cbbf3bfc8302c8a03d47abd72bb
Shtekata <gesheval@gmail.com>
2024-05-27 21:36:59 +0300

    Add Edit Invoice

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -13,15 +13,25 @@ const FormSchema = z.object({
   time: z.string()
 })
 
-const CreateInvoice = FormSchema.omit({ id: true, date: true, time: true })
-
-export async function createInvoice(formData: FormData) {
-  const { customerId, amount, status } = CreateInvoice.parse({
+function Validate(formData: FormData) {
+  const ZodInvoice = FormSchema.omit({ id: true, date: true, time: true })
+  const { customerId, amount, status } = ZodInvoice.parse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status')
   })
   const amountInCents = Math.round(amount * 100)
+  return { customerId, amountInCents, status }
+}
+
+function Revalidate() {
+  revalidatePath('/dashboard/invoices')
+  redirect('/dashboard/invoices')
+}
+
+export async function createInvoice(formData: FormData) {
+  const { customerId, amountInCents, status } = Validate(formData)
+
   const dateAndTime = new Date().toISOString().split('T')
   const date = dateAndTime[0]
   const time = dateAndTime[1].split('.')[0]
@@ -30,6 +40,15 @@ export async function createInvoice(formData: FormData) {
     insert into invoices (customer_id,amount,status,date,time)
     values (${customerId},${amountInCents},${status},${date},${time})`
 
-  revalidatePath('/dashboard/invoices')
-  redirect('/dashboard/invoices')
+  Revalidate()
+}
+
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amountInCents, status } = Validate(formData)
+
+  await sql`update invoices
+    set customer_id=${customerId},amount=${amountInCents},status=${status}
+    where id=${id}`
+
+  Revalidate()
 }
