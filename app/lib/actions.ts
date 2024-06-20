ff8cc54660484b45700e3048cb3f124fed9dc26c
Shtekata <gesheval@gmail.com>
2024-06-20 18:26:57 +0300

    Add useActionState

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,38 +4,58 @@ import { sql } from './sql-postgres-client'
 import { revalidatePath } from 'next/cache'
 import { redirect } from 'next/navigation'
 
+export type State = {
+  errors?: {
+    customerId?: string[]
+    amount?: string[]
+    status?: string[]
+  }
+  message?: string | null
+}
+
 const FormSchema = z.object({
   id: z.string(),
-  customerId: z.string(),
-  amount: z.coerce.number(),
-  status: z.enum(['pending', 'paid']),
+  customerId: z.string({ invalid_type_error: 'Please select a customer.' }),
+  amount: z.coerce
+    .number()
+    .gt(0, { message: 'Please enter an amount greater than $0.' }),
+  status: z.enum(['pending', 'paid'], {
+    invalid_type_error: 'Please select an invoice status.'
+  }),
   date: z.string(),
   time: z.string()
 })
 
 function Validate(formData: FormData) {
+  let error: State = { errors: {}, message: '' }
   const ZodInvoice = FormSchema.omit({ id: true, date: true, time: true })
-  const { customerId, amount, status } = ZodInvoice.parse({
+  const validatedFields = ZodInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status')
   })
+
+  if (!validatedFields.success) {
+    console.log(validatedFields.error.flatten().fieldErrors)
+    error = {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Create Invoice.'
+    }
+    return { customerId: '', amountInCents: '', status: '', error }
+  }
+
+  const { customerId, amount, status } = validatedFields.data
   const amountInCents = Math.round(amount * 100)
-  return { customerId, amountInCents, status }
-}
-
-function Revalidate() {
-  revalidatePath('/dashboard/invoices')
-  redirect('/dashboard/invoices')
-}
-
-export async function createInvoice(formData: FormData) {
-  const { customerId, amountInCents, status } = Validate(formData)
-
   const dateAndTime = new Date().toISOString().split('T')
   const date = dateAndTime[0]
   const time = dateAndTime[1].split('.')[0]
+  return { customerId, amountInCents, status, error, date, time }
+}
 
+export async function createInvoice(prevState: State, formData: FormData) {
+  const { customerId, amountInCents, status, error, date, time } =
+    Validate(formData)
+  if (error) return error
   try {
     await sql`
     insert into invoices (customer_id,amount,status,date,time)
@@ -44,12 +64,13 @@ export async function createInvoice(formData: FormData) {
     return { message: 'Database Error: Failed to Create Invoice.' }
   }
 
-  Revalidate()
+  revalidatePath('/dashboard/invoices')
+  redirect('/dashboard/invoices')
 }
 
 export async function updateInvoice(id: string, formData: FormData) {
-  const { customerId, amountInCents, status } = Validate(formData)
-
+  const { customerId, amountInCents, status, error } = Validate(formData)
+  if (error) return { ...error }
   try {
     await sql`update invoices
     set customer_id=${customerId},amount=${amountInCents},status=${status}
@@ -58,7 +79,8 @@ export async function updateInvoice(id: string, formData: FormData) {
     return { message: 'Database error: Failed to Update Invoice.' }
   }
 
-  Revalidate()
+  revalidatePath('/dashboard/invoices')
+  redirect('/dashboard/invoices')
 }
 
 export async function deleteInvoice(id: string) {
